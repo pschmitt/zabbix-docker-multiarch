e6cb94cfaf15bab6da798cc882682c5d98be2d2b
Philipp Schmitt <philipp@schmitt.co>
2020-03-04 11:48:57 +0100

    Add Travis build script

---
+++ travis-build.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: $(basename "$0") [MAX_TAGS]"
+}
+
+list_targets() {
+  local tmpdir
+  tmpdir="$(mktemp -d)"
+  git clone -q --depth 1 https://github.com/zabbix/zabbix-docker "$tmpdir"
+  find "$tmpdir" -maxdepth 2 -mindepth 2 -type d | grep -v .git | \
+    sed -e "s|^${tmpdir}/||" -e 's|/|-|' | sort
+  trap "rm -rf \"$tmpdir\"" EXIT
+}
+
+list_remote_tags() {
+  git ls-remote --tags "$1" | \
+    awk '{print $2}' | \
+    sed 's|refs/tags/||' | \
+    sort -r
+}
+
+travis_build() {
+  local target
+  local git_tags
+  local git_tag
+  local jobs_def=""
+  local tmp
+
+  git_tags=$(list_remote_tags https://github.com/zabbix/zabbix-docker)
+  if [[ -n "$1" ]]
+  then
+    git_tags=$(head -n "$1" <<< "$git_tags")
+  fi
+
+  # Limit to the last 3 tags
+  git_tags=$(head -n 3 <<< "$git_tags" 2>/dev/null)
+
+  for target in $(list_targets)
+  do
+    for git_tag in $git_tags
+    do
+      tmp='"TARGET='"$target"' GIT_TAG='"$git_tag"'"'
+      if [[ -n "$jobs_def" ]]
+      then
+        jobs_def="$jobs_def, $tmp"
+      else
+        jobs_def="$tmp"
+      fi
+    done
+  done
+
+  local body='{
+   "request": {
+   "message": "Auto build",
+   "branch": "'"$(git rev-parse --abbrev-ref HEAD)"'",
+   "config": {
+     "env": {
+       "jobs": ['"$jobs_def"']
+     }
+    }
+  }}'
+
+  curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token $TRAVIS_TOKEN" \
+    -d "$body" \
+    https://api.travis-ci.com/repo/pschmitt%2Fzabbix-docker-armhf/requests
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  if [[ -z "$TRAVIS_TOKEN" ]]
+  then
+    echo "TRAVIS_TOKEN is not set." >&2
+    echo "You can retrieve it with $ travis token --com" >&2
+    exit 2
+  fi
+
+  case "$1" in
+    help|h|-h|--help)
+      usage
+      exit 0
+      ;;
+  esac
+
+  travis_build "$1"
+fi
