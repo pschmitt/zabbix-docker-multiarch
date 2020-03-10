42b7204e62fb86d2e2e6609e69c22205e67c5c2a
Philipp Schmitt <philipp@schmitt.co>
2020-03-10 14:27:53 +0100

    Streamline Travis CI config (by externalizing the buildx install/setup)

--- build.sh
+++ build.sh
@@ -38,81 +38,6 @@ is_latest_git_patch() {
   [[ "$(git tag -l | grep '^'"${minor}"'\.' | sort -n | tail -1)" == "$1" ]]
 }
 
-install_latest_buildx() {
-  local arch
-  local version=0.3.1
-  local buildx_path=~/.docker/cli-plugins/docker-buildx
-
-  if [[ -x "$buildx_path" ]]
-  then
-    return
-  fi
-
-  case "$(uname -m)" in
-    x86_64)
-      arch=amd64
-      ;;
-    aarch64)
-      arch=arm64
-      ;;
-    armv6l|arm)
-      arch=arm-v6
-      ;;
-    armv7l|armhf)
-      arch=arm-v7
-      ;;
-    *)
-      arch="$(uname -m)"
-      ;;
-  esac
-  mkdir -p "$(dirname "$buildx_path")"
-  curl -L -o "$buildx_path" \
-    "https://github.com/docker/buildx/releases/download/v${version}/buildx-v${version}.linux-${arch}"
-  chmod +x "$buildx_path"
-}
-
-debug_info() {
-  env
-  docker version
-  docker buildx ls
-  docker buildx inspect
-  ls -1 /proc/sys/fs/binfmt_misc
-}
-
-setup_buildx() {
-  case "$(uname -m)" in
-    x86_64|i386)
-      docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-      # docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
-      ;;
-  esac
-
-  # CI
-  if [[ "$GITHUB_ACTIONS" == "true" ]] || [[ "$TRAVIS" == "true" ]]
-  then
-    if [[ "$GITHUB_WORKFLOW" == "Self-hosted build" ]]
-    then
-      apt-get update
-      apt-get install -y ssh
-      ssh-keyscan picopi >> ~/.ssh/known_hosts
-      docker context create picopi --docker "host=ssh://pschmitt@picopi.lan"
-      docker buildx create --use --name farm --platform linux/arm/v6,linux/arm/v7 picopi
-      docker buildx create --append --name farm default
-    else
-      docker buildx create \
-        --use \
-        --name builder \
-        --node builder \
-        --driver docker-container \
-        --driver-opt network=host
-    fi
-  fi
-  docker buildx inspect --bootstrap
-
-  # Debug info for buildx and multiarch support
-  debug_info
-}
-
 get_available_architectures() {
   local image="$1"
   local tag="${2:-latest}"
@@ -450,14 +375,6 @@ then
       ;;
   esac
 
-  # buildx setup
-  export DOCKER_CLI_EXPERIMENTAL=enabled
-  export PATH="${PATH}:~/.docker/cli-plugins"
-  if ! [[ -x ~/.docker/cli-plugins/docker-buildx ]]
-  then
-    install_latest_buildx
-    setup_buildx
-  fi
   if ! docker buildx version >/dev/null
   then
     echo "buildx is not available" >&2
